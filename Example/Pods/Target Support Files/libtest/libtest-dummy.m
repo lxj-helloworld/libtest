d5574deb6c07832e7ed7c48cf63caeb0e6ddef4d
李效晋 <lixiaojin@topscomm.com>
2019-05-25 11:11:41 +0800

    第一次提交

---
+++ Example/Pods/Target Support Files/libtest/libtest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_libtest : NSObject
+@end
+@implementation PodsDummy_libtest
+@end
---
+++ FirstApp.swift
@@ -0,0 +1,14 @@
+//
+//  FirstApp.swift
+//  FBSnapshotTestCase
+//
+//  Created by topscommmac01_lixiaojin on 2019/5/25.
+//
+
+import Foundation
+
+class FirstApp{
+    class func testMethod() {
+        print("topscommmac01_lixiaojin")
+    }
+}
