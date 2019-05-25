679ac35bbbefddb461a966fd96b666e3c23cb5c9
李效晋 <lixiaojin@topscomm.com>
2019-05-25 15:38:05 +0800

    aa

---
+++ Example/Pods/libtest/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  FBSnapshotTestCase
+//
+//  Created by topscommmac01_lixiaojin on 2019/5/25.
+//
+
+import Foundation
+
+class File{
+    let test = "dsfsfsf"
+}
--- Example/Pods/libtest/libtest/Classes/UIColorUtils.swift
+++ Example/Pods/libtest/libtest/Classes/UIColorUtils.swift
@@ -12,7 +12,7 @@ import UIKit
 extension UIColor {
     
     //  把hex颜色转为UIColor
-    class func colorWithHexString(hex:String,alpha: CGFloat = 1) ->UIColor {
+    public class func colorWithHexString(hex:String,alpha: CGFloat = 1) ->UIColor {
         var cString: String = hex.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines) //去掉空格及换行
         if cString.count < 6 {
             return UIColor.black
@@ -45,7 +45,7 @@ extension UIColor {
     }
     
    //颜色转换为图片
-   class func imageFromColor(color: UIColor, viewSize: CGSize) -> UIImage{
+  public class func imageFromColor(color: UIColor, viewSize: CGSize) -> UIImage{
         let rect: CGRect = CGRect(x: 0, y: 0, width: viewSize.width, height: viewSize.height)
         UIGraphicsBeginImageContext(rect.size)
         let context: CGContext = UIGraphicsGetCurrentContext()!
@@ -59,7 +59,7 @@ extension UIColor {
 }
 
 extension CGColor{
-    class func CGColorFromRGB(rgbValue: UInt) -> CGColor {
+    public class func CGColorFromRGB(rgbValue: UInt) -> CGColor {
         return UIColor(
             red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
             green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
